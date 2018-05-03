f8e3c65340d52d4694a444a234fb3bdc5e52d40c
IselaDiaz <isdm19@gmail.com>
2018-05-03 01:36:54 -0400

    right!

--- src/node/AINode.java
+++ src/node/AINode.java
@@ -106,7 +106,8 @@ public class AINode {
 	}
 
     public void updateTotalScore(int winID) {
-        if (this.state.getBoard().getMyId() != winID)
+        //if (this.state.getBoard().getMyId() != winID)
+    	if (this.state.getBoard().getMyId() != winID)
             totalScore++;
     }
 
